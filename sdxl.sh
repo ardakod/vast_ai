0466e1d2edd6f1c245c4de79820f7dcacfbf2574
cloud <>
2025-08-09 18:21:03 +0100

    update

---
+++ sdxl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+model="sdxl"
+script="/data/${model}.sh"
+
+curl -H "Authorization: token $GITHUB_TOKEN" -o "$script" "https://raw.githubusercontent.com/ardakod/vastai/master/${model}.sh"
+
+bash "$script"
+
+rm "$script"
